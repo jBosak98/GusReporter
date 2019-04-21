2240d16e816df071571486bf7506d3e5ba2cfa24
jBosak98 <jbosak98@gmail.com>
2019-04-21 10:19:41 +0200

    CompanyServiceImpl refactor, add html and css file

--- src/main/java/jbosak/gus/reporter/gusReporter/services/CompanyServiceImpl.java
+++ src/main/java/jbosak/gus/reporter/gusReporter/services/CompanyServiceImpl.java
@@ -7,7 +7,6 @@ import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.stereotype.Service;
 import org.springframework.xml.transform.StringSource;
 import org.tempuri.IUslugaBIRzewnPubl;
-import org.xml.sax.SAXParseException;
 
 import javax.xml.bind.JAXBElement;
 import javax.xml.namespace.QName;
@@ -30,11 +29,12 @@ public class CompanyServiceImpl implements CompanyService {
         ParametryWyszukiwania regonA = new ParametryWyszukiwania();
         regonA.setNip(new JAXBElement<>(new QName("http://CIS/BIR/PUBL/2014/07/DataContract", "Nip"),String.class, nip));
 
-        Jaxb2Marshaller unmarshaller = (Jaxb2Marshaller) soapService.getUnmarshaller();
         String xmlResponse = service.daneSzukaj(regonA);
-        if (xmlResponse.equals(""))
+        if (xmlResponse.equals("")) {
             return  null;
+        }
 
+        Jaxb2Marshaller unmarshaller = (Jaxb2Marshaller) soapService.getUnmarshaller();
         CompanyData c = (CompanyData) unmarshaller.unmarshal(new StringSource(xmlResponse));
         return c.dane;
     }
